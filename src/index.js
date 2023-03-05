3503ce205e732f10eac66fd76f19a09df13641e1
nguyenhoanglong1006 <longsupper1704@gmail.com>
2023-03-05 23:05:46 +0700

    #1 - long update footer

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import 'react-slideshow-image/dist/styles.css';
 import GlobalStyles from '~/components/GlobalStyles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
